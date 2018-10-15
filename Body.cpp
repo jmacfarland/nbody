38adbb6cfb3de92441a6301561919173ec62fe5c
BobbyBytes <robbie234@gmail.com>
2018-10-14 20:31:32 -0400

    Tried alot of stuff that didnt work, working on scaling the universe now got atleast somewhere..

--- Body.cpp
+++ Body.cpp
@@ -1,7 +1,7 @@
 #include "Body.hpp"
-
+#include <iostream>
 Body::Body(float x_pos, float y_pos, float x_vel, float y_vel, float mass_init, std::string filename) {
-  position.x = x_pos;
+  position.x = x_pos; //lets assume were in a 600 x 600 window to get this baby at the origin
   position.y = y_pos;
   velocity.x = x_vel;
   velocity.y = y_vel;
@@ -29,9 +29,17 @@ void Body::setPos(sf::Vector2f newPos) {
 void Body::setVel(sf::Vector2f newVel) {
   velocity = newVel;
 }
-
+//setup sprite now also lets the location as well as the filename.
 void Body::setupSprite(std::string filename)
 {
   texture.loadFromFile(filename);
   sprite.setTexture(texture);
+  sprite.setPosition(position); 
+  
+}
+void Body::setRelativePosition(float universeSize){
+
+  position.x = ((position.x / universeSize) * 600) + 300;
+  position.y = ((position.y / universeSize) * 600) + 300;
+  sprite.setPosition(position);    
 }
