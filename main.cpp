711177f7fcf923e6d3be3110076e476cbe53abbc
jmacfarland <macfarland.jamison@gmail.com>
2018-10-22 12:40:39 -0400

    Added displayResults function, removed extra printing code

--- main.cpp
+++ main.cpp
@@ -5,23 +5,32 @@
 
 #include "Body.hpp"
 
+void displayResults(std::vector<Body*> bodies, int numberOfBodies, float universeSize) {
+  std::cout << numberOfBodies << std::endl;
+  std::cout << universeSize << std::endl;
+  for(int i = 0; i < static_cast<int>(bodies.size()); ++i) {
+    sf::Vector2f position = bodies[i]->getPos();
+    std::cout << position.x << " " << position.y << " ";
+    sf::Vector2f velocity = bodies[i]->getVel();
+    std::cout << velocity.x << " " << velocity.y << " ";
+    std::cout << bodies[i]->getMass() << " " << bodies[i]->getFilename() << std::endl;
+  }
+}
+
 int main(int argc, char *argv[]) {
   //double totalseconds;
   double deltaTseconds;
-  
+
   //totalseconds = atof(argv[1]);
   deltaTseconds = atof(argv[2]);
 
   int numberOfBodies;
-  std::cout << "delta T seconds" << deltaTseconds <<"\n";
   float universeSize;
   std::vector<Body *> bodies;
   std::string line;
   // read in universe size and number of bodies
   std::cin >> numberOfBodies;
-  std::cout << "number of bodies: " << numberOfBodies << std::endl;
   std::cin >> universeSize;
-  std::cout << universeSize << std::endl;
   // read in the planet data
 
   // 600x600 window
@@ -39,14 +48,11 @@ int main(int argc, char *argv[]) {
   std::string filename;
   for (int i = 0; i < numberOfBodies; i++) {
     std::cin >> xpos;
-    std::cout << "X: " << xpos << ", ";
     std::cin >> ypos;
-    std::cout << "Y: " << ypos << ", ";
     std::cin >> xvel;
     std::cin >> yvel;
     std::cin >> mass;
     std::cin >> filename;
-    std::cout << filename << std::endl;
     bodies.push_back(new Body(xpos, ypos, xvel, yvel, mass, filename));
     bodies[i]->setMyOrigin();
     bodies[i]->setUniverseSize(universeSize);
@@ -60,7 +66,7 @@ int main(int argc, char *argv[]) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    
+
     window.clear();
     window.draw(backgroundSprite);
     for( int i = 0; i < numberOfBodies; i++){ // i is the body to work on
@@ -68,7 +74,7 @@ int main(int argc, char *argv[]) {
         double xForceTotal = 0;
         double accelx, accely, mass1, mass2;
         for ( int j = 0; j < numberOfBodies; j++){// j iterates through all the other bodies
-        
+
         sf::Vector2f tempvector, tempvector2, newVelocity;
         //declare and label a bunch of floats for use here
         double CONSTANT_G = 0.00000000006673;
@@ -78,22 +84,21 @@ int main(int argc, char *argv[]) {
         tempvector2 = bodies[j] -> getPos();
         xdis = tempvector.x - tempvector2.x;
         ydis = tempvector.y - tempvector2.y;
-        xdissqrd = xdis * xdis; 
+        xdissqrd = xdis * xdis;
         ydissqrd = ydis * ydis;
         temp = xdissqrd + ydissqrd; // a square + b square
         distanceBetween = sqrt(temp); // c square
-       
+
         //calculate the force between the two objects
-        double force; 
+        double force;
         mass1 = bodies[i] -> getMass();
-        mass2 = bodies[j] -> getMass(); 
+        mass2 = bodies[j] -> getMass();
         distanceBetweensqrd = distanceBetween * distanceBetween;
         if(distanceBetweensqrd == 0){
           force = 0;
           forcex = 0;
           forcey = 0;
-        }
-        else {
+        } else {
           force = (CONSTANT_G * mass1 * mass2) / distanceBetweensqrd;
           //calculate the x and y components of the force ont th body
           forcex = (xdis / distanceBetween) * force;
@@ -101,27 +106,28 @@ int main(int argc, char *argv[]) {
         }
         xForceTotal = xForceTotal + forcex;
         yForceTotal = yForceTotal + forcey;
-        
-  
+
+
         }//end j
     // calculate the acceleration on the body's x and y components
         accelx = xForceTotal / mass1;
-        accely = yForceTotal / mass1; 
+        accely = yForceTotal / mass1;
         //give the new accel to the body
         bodies[i] ->setAccel(accelx, accely);
-    
-    
+
+
     }//end i
-     
+
       for (int i = 0; i < (int)bodies.size(); i++) {
 
-        bodies[i] ->printData();
+        //bodies[i] ->printData();
         window.draw(*bodies.at(i));
         bodies[i] -> step(deltaTseconds);
-        bodies[i] ->printData();
+        //bodies[i] ->printData();
       }
     window.display();
   }
+  displayResults(bodies, numberOfBodies, universeSize);
 
   return 0;
 }
